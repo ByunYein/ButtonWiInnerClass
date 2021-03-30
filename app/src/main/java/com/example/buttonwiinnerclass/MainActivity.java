b0ced946819446171283b9bb3aa5b85790bfd7aa
ByunYein <dpdls311@gmail.com>
2021-03-30 11:44:25 +0900

    one listener for two buttons

--- app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
+++ app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
@@ -17,23 +17,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton1 = findViewById(R.id.button1);
+        mButton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(new MyOnClickListener());
-        mButton2.setOnClickListener(new MyOnClickListener2());
+        mButton2.setOnClickListener(new MyOnClickListener());
     }
 
         private class MyOnClickListener implements View.OnClickListener {
             @Override
             public void onClick(View v) {
-                mTextView1.setText("You clicked 변예인의 Button");
+                switch(v.getId()){
+                    case R.id.button1: mTextView1.setText("You clicked 변예인의 Button"); break;
+
+                    case R.id.button2: mTextView1.setText("You clicked my 2nd Button"); break;
+                    default: break;
             }
         }
-    private class MyOnClickListener2 implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            mTextView1.setText("You clicked my 2nd Button");
-        }
+
     }
 
 
